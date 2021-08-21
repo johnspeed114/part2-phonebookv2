4f101e5f20550a80c56ff38c4d19315df412ad3e
John Law <johnlaw114@gmail.com>
2021-08-21 19:30:43 +0900

    explained added

--- src/App.js
+++ src/App.js
@@ -5,15 +5,11 @@ import personService from './services/persons'
 function App() {
   //  const [newNote, setNewNote] = useState('')
   const [persons, setPersons] = useState([]) //persons,  name convention, contact as info of the person[naming vars and objects are important to remember]
-  //    { name: 'Arto Hellas', number: '040-123456' },
-  // { name: 'Ada Lovelace', number: '39-44-5323523' },
-  // { name: 'Dan Abramov', number: '12-43-234345' },
-  // { name: 'Mary Poppendieck', number: '39-23-6423122' } [IMPORTANT] NO need for these objects since I added the objects into db.json and used useEffect from server to add to
-  //useState of persons!!!
+
   const [newName, setNewName] = useState('') //better to put these two lines as one grouped object
   const [newNum, setNewNum] = useState()
   useEffect(() => {
-    personService.getAll().then(initialPersons => {
+    personService.getAll().then((initialPersons) => {
       setPersons(initialPersons)
     })
   }, []) //first useEffect paramater is the function, 2nd is the frequency of this function(like when and how many times to run)
@@ -31,44 +27,55 @@ function App() {
   const addContacts = (event) => {
     event.preventDefault()
     console.log(persons[0].name)
-    const duplicatePerson = persons.find(p => p.name === newName)
-    if ((typeof duplicatePerson !== 'undefined' && duplicatePerson.number !== newNum)) {
-      const changedNumb = { name: duplicatePerson.name, number: newNum }
-      personService.update(duplicatePerson.id, changedNumb).then(returnedNum => {
-        if (window.confirm(`${returnedNum.name} is already added to phonebook,
-         replace the old number with new one?`)) {
-            setPersons(persons.map(person =>
-               person.id !== duplicatePerson.id ? person : returnedNum)) }
-        setNewName('')
-        setNewNum('')
-        
-
-      })
+    const duplicatePerson = persons.find((p) => p.name === newName) //we need to first find if there are any same names in the contact with the find function
+    if (
+      typeof duplicatePerson !== 'undefined' &&
+      duplicatePerson.number !== newNum
+    ) {
+      //check if are any duplicates through typeof, and the duplicate's number is not the same for the possibility of the next part
+      const changedNumb = { name: duplicatePerson.name, number: newNum } //here, we save the changed number for the same person
+      personService
+        .update(duplicatePerson.id, changedNumb)
+        .then((returnedNum) => {
+          if (
+            window.confirm(`${returnedNum.name} is already added to phonebook,
+         replace the old number with new one?`)
+          ) {
+            setPersons(
+              persons.map((person) =>
+                person.id !== duplicatePerson.id ? person : returnedNum
+              )
+            )
+          } //here, we just copy old copy of the save state objects and just update the new number of duplicate person to the state
+          setNewName('')
+          setNewNum('')
+        })
       return
     } else if (typeof duplicatePerson !== 'undefined') {
+      //function here is for just checkin both the same number and person when adding contacts. No need number check since the first conditional checks it
       console.log('test')
       alert(`${newName} is already added to phonebook`)
       setNewName('')
       setNewNum('')
-      return 
-
+      return
     }
 
     const personsObject = {
       name: newName,
       number: newNum,
     }
-    personService.create(personsObject).then(returnedPerson => {
+    personService.create(personsObject).then((returnedPerson) => {
       setPersons(persons.concat(returnedPerson))
       setNewName('')
       setNewNum('')
     })
-
   }
 
-  const deleteButton = name => {
-    personService.remove(name)
-    setPersons(persons.filter(person => person.id !== name.id))
+  const deleteButton = (name) => {
+    personService.remove(name) //the name parameter, here, are each of the individual contacts through the map array from persons state, then remove function communicates with the backend
+    //and uses the name's id to find and deletes the contact item.
+    setPersons(persons.filter((person) => person.id !== name.id)) //since the backend update doesn't immediately change the frontend ui, each contact(name) has to be filtered to check
+    //which item is as the same as the name to be deleted and removed from the frontend
   }
 
   return (
